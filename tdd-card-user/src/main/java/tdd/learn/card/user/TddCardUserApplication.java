1d0242c539d8376c6cc0e3b6a1756f72b4d519b5
Hao Peng <hao.peng@thoughtworks.com>
2022-08-16 11:13:12 +0800

    feat: init repo

---
+++ tdd-card-user/src/main/java/tdd/learn/card/user/TddCardUserApplication.java
@@ -0,0 +1,11 @@
+package tdd.learn.card.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddCardUserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TddCardUserApplication.class);
+    }
+}
---
+++ tdd-registration/src/main/java/tdd/learn/registration/TddRegistrationApplication.java
@@ -0,0 +1,11 @@
+package tdd.learn.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddRegistrationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TddRegistrationApplication.class);
+    }
+}
