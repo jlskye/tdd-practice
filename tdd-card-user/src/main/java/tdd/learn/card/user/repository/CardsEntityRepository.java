1b411ac3b6af02953e4dba8c6a5535e41930325d
lan.jiang <lan.jiang@thoughtworks.com>
2022-09-12 16:31:31 +0800

    test: add postgres test container configuration

---
+++ tdd-card-user/src/main/java/tdd/learn/card/user/repository/CardsEntityRepository.java
@@ -0,0 +1,10 @@
+package tdd.learn.card.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import tdd.learn.card.user.entity.Card;
+
+@Repository
+public interface CardsEntityRepository extends JpaRepository<Card, Long> {
+    Card getCardsById(Long id);
+}
---
+++ tdd-card-user/src/test/java/tdd/learn/card/user/repository/CardsEntityRepositoryTest.java
@@ -0,0 +1,31 @@
+package tdd.learn.card.user.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import tdd.learn.card.user.entity.Card;
+
+class CardsEntityRepositoryTest extends AbstractIntegrationTest{
+    @Autowired
+    private CardsEntityRepository cardsEntityRepository;
+
+    @BeforeEach
+    void setUp() {
+        Card card = new Card(1L, "111111", "00");
+        this.cardsEntityRepository.save(card);
+    }
+
+    @AfterEach
+    void cleanUp() {
+        this.cardsEntityRepository.deleteAll();
+    }
+
+    @Test
+    void should_retrieve_card_by_id() {
+        Long id = 1L;
+        Card card = cardsEntityRepository.getCardsById(id);
+        Assertions.assertThat(card).isEqualTo(card);
+    }
+}
